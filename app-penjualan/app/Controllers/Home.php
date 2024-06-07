60068e60e9af169d5ea90cf9d9d480d714a4e236
Bagas <124764725+Chibie345@users.noreply.github.com>
2024-06-07 09:35:49 +0700

    Update Home.php

--- app-penjualan/app/Controllers/Home.php
+++ app-penjualan/app/Controllers/Home.php
@@ -7,8 +7,8 @@ class Home extends BaseController
     public function index(): string
     {
         $data =[
-            'title'=>'Aplikasi Penjualan Barang Minimarket',
-            'descriptions'=>'Web ini untuk transaksi penjualan barang di minimarket',
+            'title'=>'Aplikasi Penjualan Aksesoris Komputer',
+            'descriptions'=>'Web ini untuk transaksi penjualan aksesoris komputer',
             'author'=> '2022101009 - Bagas Aditya Silalahi'
         ];
         return view('v_login/login', $data);
